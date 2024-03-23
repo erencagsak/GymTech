5cd1d02838f164e0e6e666805154cf3774246b0c
Eren Taha ÇAĞŞAK <erentahacagsak@gmail.com>
2024-03-23 17:19:48 +0300

    SQLite eklendi
    SQLite eklendi, kurulum tamamlandı.

--- app/src/main/java/com/eren/gymtech/SplashScreenActivity.kt
+++ app/src/main/java/com/eren/gymtech/SplashScreenActivity.kt
@@ -4,16 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import com.eren.gymtech.activity.LoginActivity
+import com.eren.gymtech.databinding.ActivityMainBinding
+import com.eren.gymtech.global.DB
+import com.eren.gymtech.manager.SessionManager
+import java.lang.Exception
 
 class SplashScreenActivity : AppCompatActivity() {
     private var mDelayHandler: Handler? = null
     private val splash_delay: Long = 5000
+    var db : DB? = null
+    var session : SessionManager? = null
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        db = DB(this)
+        session = SessionManager(this)
+
+        insertAdminData()
         mDelayHandler = Handler()
         mDelayHandler?.postDelayed(mRunnable,splash_delay)
     }
@@ -22,5 +35,28 @@ class SplashScreenActivity : AppCompatActivity() {
         val intent = Intent(this,LoginActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    private fun insertAdminData(){
+        try {
+            val sqlCheck = "SELECT * FROM ADMIN"
+            db?.fireQuery(sqlCheck)?.use {
+                if (it.count > 0){
+                    Log.d("SplashActivity","data available")
+                }
+                else{
+                    val sqlQuery = "INSERT OR REPLACE INTO ADMIN(ID,USER_NAME,PASSWORD,MOBILE) VALUES('1','Admin','1234','8888')"
+                    db?.executeQuery(sqlQuery)
+                }
+            }
+        }
+        catch (e : Exception){
+            e.printStackTrace()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mDelayHandler?.removeCallbacks(mRunnable)
     }
 }

252fa888d400869db251cb4830e40484ff60cd3f
Eren Taha ÇAĞŞAK <erentahacagsak@gmail.com>
2024-03-29 22:25:10 +0300

    Yönetici için NavBar eklendi.
    NavBar eklendi, butonlar çalışmıyor ( daha sonra eklenecek. )

--- app/src/main/java/com/eren/gymtech/activity/AdminHomeActivity.kt
+++ app/src/main/java/com/eren/gymtech/activity/AdminHomeActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.eren.gymtech.R
 
 class AdminHomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_home)
---
+++ app/src/main/java/com/eren/gymtech/activity/NavBarActivity.kt
@@ -0,0 +1,18 @@
+package com.eren.gymtech.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.eren.gymtech.R
+
+class NavBarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_nav_bar)
+    }
+}
